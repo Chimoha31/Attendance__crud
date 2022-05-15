c08e0e15c7c4c439c9a4e56bacca49995452c392
Chimoha31 <mokochii1108@gmail.com>
2022-05-15 01:52:02 -0700

    Implement for input field

--- src/componentrs/Input.jsx
+++ src/componentrs/Input.jsx
@@ -1,16 +1,34 @@
-import React, { Fragment } from "react";
-import { Button, FormControl, InputGroup } from "react-bootstrap";
+import React, { Fragment, useState } from "react";
+import { Alert, Button, FormControl, InputGroup } from "react-bootstrap";
+import PersonData from "./person_data/PersonData";
 
 const Input = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("Remote");
+  const [flag, setFlag] = useState(false);
+  const [message, setMessage] = useState({ error: false, errorMsg: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(e);
+    if (name === "" || email === "") {
+      setMessage({error: true, errorMsg: "Fill in the All blank is required"});
+    }else{
+      await PersonData.addPersons()
+    }
+  };
+
   return (
     <Fragment>
-      <form className="mb-5">
+      <form onSubmit={handleSubmit} className="mb-5">
+        {message.errorMsg ? <Alert variant="danger">{message.errorMsg}</Alert> : <Alert></Alert>}
         <InputGroup className="mb-3">
           <InputGroup.Text id="basic-addon1">Name</InputGroup.Text>
           <FormControl
             placeholder="Name"
             aria-label="Name"
             aria-describedby="basic-addon1"
+            onChange={(e) => setName(e.target.value)}
           />
         </InputGroup>
         <InputGroup className="mb-3">
@@ -19,12 +37,31 @@ const Input = () => {
             placeholder="Email"
             aria-label="Email"
             aria-describedby="basic-addon1"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </InputGroup>
 
         <div className="mb-3">
-          <Button variant="outline-success">Remote</Button>
-          <Button variant="outline-danger">In Person</Button>
+          <Button
+            variant="outline-success"
+            onClick={(e) => {
+              setStatus("Remote");
+              setFlag(!flag);
+            }}
+            disabled={flag}
+          >
+            Remote
+          </Button>
+          <Button
+            variant="outline-danger"
+            onClick={(e) => {
+              setStatus("In person");
+              setFlag(!flag);
+            }}
+            disabled={!flag}
+          >
+            In Person
+          </Button>
         </div>
 
         <div className="d-grid">
---
+++ src/componentrs/person_data/PersonData.jsx
@@ -0,0 +1,40 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import {db} from '../firebase/Firebase';
+
+const personRef = collection(db, "persons");
+
+class PersonData {
+  addPersons = (newPerson) => {
+    return addDoc(personRef, newPerson);
+  }
+
+  updatePerson = (id, updatePerson) => {
+    const personDoc = doc(personRef, id); //エラー出たら先にここを疑う！
+    return updateDoc(personDoc, updatePerson);
+  }
+
+  deletePerson = (id) => {
+    const personDoc = doc(personRef, id);   //エラー出たら先にここを疑う！
+    return deleteDoc(personDoc);
+  }
+
+  getAllPerson = () => {
+    return getDocs(personRef);
+  }
+
+  getPerson = (id) => {
+    const personDoc = doc(personRef, id);  //エラー出たら先にここを疑う！
+    return getDoc(personDoc);
+  }
+}
+
+
+export default new PersonData();
